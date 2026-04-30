c0d8b7acf956a03e73f571f6396f2ea6eca977c1
Oleksii Piliai <alekseivpiliai@gmail.com>
2026-04-30 11:31:19 +0200

    add js css

---
+++ src/js/menu.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    // Додати атрибут data-modal на бекдроп меню
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на дів при натисканні на кнопки
+    refs.modal.classList.toggle("is-open");
+  }
+})();
+
+// <button type="button" class="btn-modal" data-modal-close> ПРИКЛАД 
