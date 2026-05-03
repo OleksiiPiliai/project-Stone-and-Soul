e0094599c1b65012578c8719d586f9ef026afc15
Oleksii Piliai <alekseivpiliai@gmail.com>
2026-05-03 23:08:18 +0200

    addJS

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,20 +1,37 @@
 (() => {
+  // Находим элементы (замените селекторы в кавычках на ваши классы)
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    // Додати атрибут data-modal на бекдроп меню
-    modal: document.querySelector("[data-modal]"),
+    openMenuBtn: document.querySelector('.burger-btn'), // Кнопка открытия
+    closeMenuBtn: document.querySelector('.menu-close-button'), // Кнопка закрытия
+    menu: document.querySelector('.mob-menu'), // Само меню
+    menuLinks: document.querySelectorAll('.menu-nav-link'), // Ссылки в меню
+    logo: document.querySelector('.menu-logo'), // Логотип в хедере
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  // Функция переключения класса is-open
+  const toggleMenu = () => {
+    refs.menu.classList.toggle('is-open');
+    // Блокируем скролл основной страницы, когда меню открыто
+    document.body.classList.toggle('no-scroll');
+  };
 
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на дів при натисканні на кнопки
-    refs.modal.classList.toggle("is-open");
+  // Функция принудительного закрытия
+  const closeMenu = () => {
+    refs.menu.classList.remove('is-open');
+    document.body.classList.remove('no-scroll');
+  };
+
+  // Слушатели событий
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрытие при клике на логотип
+  if (refs.logo) {
+    refs.logo.addEventListener('click', closeMenu);
   }
-})();
 
-// <button type="button" class="btn-modal" data-modal-close> ПРИКЛАД 
+  // Закрытие при клике на любую ссылку в меню
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
+  });
+})();
